6d2cfc5ce755007f954f817c4e21cc632a8aef60
knute <unknown>
2007-01-14 12:46:09 +0000

    Got rid of the serialization warning in LayerEvent.  Not sure if it serialization actually works. [scripted commit from darcs-repos]

--- src/org/boblycat/blimp/LayerEvent.java
+++ src/org/boblycat/blimp/LayerEvent.java
@@ -5,6 +5,9 @@ import java.util.EventObject;
 import org.boblycat.blimp.layers.Layer;
 
 public class LayerEvent extends EventObject {
+    private static final long serialVersionUID = 1L;
+
+    // TODO: make sure layer is really serializable using normal Java serialization?
     private Layer layer;
 
     public LayerEvent(Layer layer) {
