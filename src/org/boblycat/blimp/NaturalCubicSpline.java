d76a44ddca2746b9f6e8fa8de3da096876c67629
knute <unknown>
2007-02-04 15:40:41 +0000

    Removed some unused code from NaturalCubicSpline.java

--- src/org/boblycat/blimp/NaturalCubicSpline.java
+++ src/org/boblycat/blimp/NaturalCubicSpline.java
@@ -250,32 +250,4 @@ public class NaturalCubicSpline {
         }
         return values;
     }
-
-    // linear interpolation for now, fix!
-    public double old_getSplineValue(double x) {
-        // System.out.println("getSplineValue " + x);
-        if (points.size() == 0)
-            return 0;
-        if (points.size() == 1)
-            return points.values().iterator().next();
-        Iterator<Double> xiter = points.keySet().iterator();
-        double x1 = xiter.next();
-        if (x <= x1)
-            return points.get(x1);
-        double x2 = x1;
-        while (xiter.hasNext()) {
-            x2 = xiter.next();
-            if (x <= x2) {
-                double h = x - x1;
-                double y1 = points.get(x1);
-                double y2 = points.get(x2);
-                // System.out.println("h=" + h + " x=" + x + " x1=" + x1
-                // + " x2=" + x2 + " y1=" + y1 + " y2=" + y2);
-                return y1 + (y2 - y1) * h / (x2 - x1);
-            }
-            x1 = x2;
-        }
-        // System.out.println("fell through");
-        return points.get(x2);
-    }
 }
