aaaa8d6048163f9a4b9c2ed334e79e8c5e5e294e
knute <unknown>
2007-04-15 21:20:58 +0000

    Added a simple SoftHashMap implementation which uses SoftReferences for the values.  This replaces the PriorityCacheMap class in BitmapCache, and initial testing indicates that this fixes many out-of-memory errors.

--- src/org/boblycat/blimp/BitmapCache.java
+++ src/org/boblycat/blimp/BitmapCache.java
@@ -6,11 +6,7 @@ import org.boblycat.blimp.layers.AdjustmentLayer;
 import org.boblycat.blimp.layers.InputLayer;
 import org.boblycat.blimp.layers.Layer;
 
-class BitmapTable extends PriorityCacheMap<String, Bitmap> {
-    BitmapTable(int capacity) {
-        super(capacity);
-    }
-    
+class BitmapTable extends SoftHashMap<String, Bitmap> {
     void put(Layer layer, Bitmap bitmap) {
         put(Serializer.layerToXml(layer), bitmap);
     }
@@ -21,25 +17,18 @@ class BitmapTable extends PriorityCacheMap<String, Bitmap> {
 }
 
 public class BitmapCache {
-    // Note: in order to avoid a potentially exponential memory growth when editing,
-    // the capacities are currently set to 1.  Maybe rethink the caching strategy
-    // and have some sort of global priority system instead?
-    static final int INPUT_CAPACITY = 1;
-    static final int ADJUSTMENT_CAPACITIES = 1;
     BitmapTable inputTable;
-    //PriorityCacheMap<Bitmap, BitmapTable> adjustmentTables;
     WeakHashMap<Bitmap, BitmapTable> adjustmentTables;
     
     public BitmapCache() {
-        inputTable = new BitmapTable(INPUT_CAPACITY);
-        //adjustmentTables = new PriorityCacheMap<Bitmap, BitmapTable>(POOL_SIZE);
+        inputTable = new BitmapTable();
         adjustmentTables = new WeakHashMap<Bitmap, BitmapTable>();
     }
     
     private BitmapTable getAdjustmentTable(Bitmap source) {
         if (adjustmentTables.containsKey(source))
             return adjustmentTables.get(source);
-        BitmapTable newTable = new BitmapTable(ADJUSTMENT_CAPACITIES);
+        BitmapTable newTable = new BitmapTable();
         adjustmentTables.put(source, newTable);
         return newTable;
     }
---
+++ src/org/boblycat/blimp/SoftHashMap.java
@@ -0,0 +1,116 @@
+package org.boblycat.blimp;
+
+import java.lang.ref.SoftReference;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+/**
+ * A hash map in which the values are SoftReferences, which makes it suitable
+ * for memory-sensitive caches.
+ * 
+ * @author Knut Arild Erstad
+ *
+ * @param <K> The key type.
+ * @param <V> The value type.
+ */
+public class SoftHashMap<K, V> implements Map<K, V> {
+    public class Entry implements Map.Entry<K, V> {
+        //SoftReference<K> keyRef;
+        K key;
+        SoftReference<V> valueRef;
+        
+        Entry(K key, V value) {
+            //keyRef = new SoftReference<K>(key);
+            this.key = key;
+            valueRef = new SoftReference<V>(value);
+        }
+        
+        public K getKey() {
+            //return keyRef.get();
+            return key;
+        }
+        
+        public V getValue() {
+            return valueRef.get();
+        }
+        
+        public V setValue(V value) {
+            SoftReference<V> oldRef = valueRef;
+            valueRef = new SoftReference<V>(value);
+            return oldRef.get();
+        }
+    }
+    
+    HashMap<K, Entry> map;
+    
+    private final V entryValue(Entry entry) {
+        if (entry == null)
+            return null;
+        return entry.getValue();
+    }
+    
+    public SoftHashMap() {
+        map = new HashMap<K, Entry>();
+    }
+    
+    public void clear() {
+        map.clear();
+    }
+
+    public boolean containsKey(Object key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        for (Entry e: map.values()) {
+            if (value.equals(e.getValue()))
+                return true;
+        }
+        return false;
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        return new HashSet<Map.Entry<K,V>>(map.values());
+    }
+
+    public V get(Object key) {
+        return entryValue(map.get(key));
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public Set<K> keySet() {
+        return map.keySet();
+    }
+
+    public V put(K key, V value) {
+       return entryValue(map.put(key, new Entry(key, value)));
+    }
+
+    public void putAll(Map<? extends K, ? extends V> otherMap) {
+        for (Map.Entry<? extends K, ? extends V> entry: otherMap.entrySet()) {
+            put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public V remove(Object key) {
+        return entryValue(map.remove(key));
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public Collection<V> values() {
+        Vector<V> vec = new Vector<V>(size());
+        for (Entry entry: map.values())
+            vec.add(entry.getValue());
+        return vec;
+    }
+}
