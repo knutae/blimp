9ba3fcacdce2d62bd6059878890b1ddfbb93d743
knute <unknown>
2007-04-22 18:47:20 +0000

    Added vertical and horizontal flip parameters to OrientationLayer

--- src/org/boblycat/blimp/layers/OrientationLayer.java
+++ src/org/boblycat/blimp/layers/OrientationLayer.java
@@ -1,6 +1,8 @@
 package org.boblycat.blimp.layers;
 
 import net.sourceforge.jiu.data.PixelImage;
+import net.sourceforge.jiu.geometry.Flip;
+import net.sourceforge.jiu.geometry.Mirror;
 import net.sourceforge.jiu.geometry.Rotate180;
 import net.sourceforge.jiu.geometry.Rotate90Left;
 import net.sourceforge.jiu.geometry.Rotate90Right;
@@ -16,6 +18,10 @@ public class OrientationLayer extends AdjustmentLayer {
     }
     
     private Rotation rotation;
+    
+    boolean flipHorizontal;
+    
+    boolean flipVertical;
     
     public OrientationLayer() {
         rotation = Rotation.None;
@@ -37,6 +43,10 @@ public class OrientationLayer extends AdjustmentLayer {
             image = applyJiuOperation(image, new Rotate180());
             break;
         }
+        if (flipVertical)
+            image = applyJiuOperation(image, new Flip());
+        if (flipHorizontal)
+            image = applyJiuOperation(image, new Mirror());
         return new Bitmap(image);
     }
 
@@ -51,6 +61,22 @@ public class OrientationLayer extends AdjustmentLayer {
 
     public Rotation getRotation() {
         return rotation;
+    }
+
+    public void setFlipVertical(boolean flip) {
+        this.flipVertical = flip;
+    }
+
+    public boolean getFlipVertical() {
+        return flipVertical;
+    }
+
+    public void setFlipHorizontal(boolean flip) {
+        this.flipHorizontal = flip;
+    }
+
+    public boolean getFlipHorizontal() {
+        return flipHorizontal;
     }
 
 }
