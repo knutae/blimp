3bafee60f183385c4df3c08b7578d8091af35bfe
knute <unknown>
2009-01-03 14:20:13 +0000

    Moved SuperSamplingScaleOperation from ViewResizeLayer to jiuops package.

--- src/org/boblycat/blimp/layers/ViewResizeLayer.java
+++ src/org/boblycat/blimp/layers/ViewResizeLayer.java
@@ -18,112 +18,10 @@
  */
 package org.boblycat.blimp.layers;
 
-import java.util.Arrays;
-
 import org.boblycat.blimp.Bitmap;
 import org.boblycat.blimp.BitmapSize;
 import org.boblycat.blimp.ZoomFactor;
-
-import net.sourceforge.jiu.data.IntegerImage;
-import net.sourceforge.jiu.data.PixelImage;
-import net.sourceforge.jiu.ops.ImageToImageOperation;
-import net.sourceforge.jiu.ops.MissingParameterException;
-import net.sourceforge.jiu.ops.WrongParameterException;
-
-/**
- * Helper class that provides a memory-efficient upscaled view of an integer image.
- * No more than one line is upscaled at a time.
- */
-class UpScaledImageWrapper {
-    int scale;
-    int width, height;
-    int lastY;
-    int[] buffer;
-    IntegerImage image;
-    UpScaledImageWrapper(IntegerImage image, int scale) {
-        assert(scale >= 1);
-        this.image = image;
-        this.scale = scale;
-        width = image.getWidth() * scale;
-        height = image.getHeight() * scale;
-        lastY = -1;
-        if (scale > 1)
-            buffer = new int[width];
-    }
-    
-    void getLine(int channelIndex, int y, int[] dest) {
-        assert(dest.length >= width);
-        if (scale == 1) {
-            // optimize this very common case
-            image.getSamples(channelIndex, 0, y, width, 1, dest, 0);
-            return;
-        }
-        if (y / scale != lastY) {
-            image.getSamples(channelIndex, 0, y / scale, image.getWidth(), 1, buffer, 0);
-            // Duplicate all samples in the line "scale" times.
-            // This is possible to do in-place by starting from the right.
-            for (int x = width-1; x >= 0; x--) {
-                buffer[x] = buffer[x / scale];
-            }
-            lastY = y / scale;
-        }
-        System.arraycopy(buffer, 0, dest, 0, width);
-    }
-}
-
-/**
- * A simple super-sampling operation for scaling integer images using a ratio
- * (rational number).
- * This is relatively fast and gives a decent antialiasing effect.
- */
-class SuperSamplingScaleOperation extends ImageToImageOperation {
-    int multiplier;
-    int divisor;
-    
-    public void process() throws MissingParameterException,
-    WrongParameterException {
-        PixelImage pInput = getInputImage();
-        if (pInput == null)
-            throw new MissingParameterException("missing input image");
-        if (!(pInput instanceof IntegerImage))
-            throw new WrongParameterException("unsupported image type: must be IntegerImage");
-        if (divisor <= 0)
-            throw new WrongParameterException("unsupported downscale factor, must be >= 1");
-        if (multiplier <= 0)
-            throw new WrongParameterException("unsupported upscale factor, must be >= 1");
-        IntegerImage input = (IntegerImage) pInput;
-        // Note: can currently drop a few pixels at the edges, but that should be acceptable
-        int inWidth = input.getWidth() * multiplier;
-        int inHeight = input.getHeight() * multiplier; 
-        int outWidth = inWidth / divisor;
-        int outHeight = inHeight / divisor;
-        IntegerImage output = (IntegerImage) input.createCompatibleImage(outWidth, outHeight);
-        int[] outLine = new int[outWidth];
-        int[] inLine = new int[inWidth];
-        UpScaledImageWrapper inWrapper = new UpScaledImageWrapper(input, multiplier);
-        int divisor2 = divisor * divisor;
-        for (int channel=0; channel<input.getNumChannels(); channel++) {
-            for (int y=0; y<outHeight; y++) {
-                Arrays.fill(outLine, 0);
-                int startY = y*divisor;
-                int endY = Math.min((y+1)*divisor, inHeight);
-                for (int inY=startY; inY<endY; inY++) {
-                    inWrapper.getLine(channel, inY, inLine);
-                    int endX = outWidth * divisor;
-                    for (int inX=0; inX < endX; inX++) {
-                        outLine[inX / divisor] += inLine[inX];
-                    }
-                }
-                for (int x=0; x<outWidth; x++) {
-                    outLine[x] /= divisor2;
-                }
-                output.putSamples(channel, 0, y, outWidth, 1, outLine, 0);
-                setProgress(y, outHeight);
-            }
-        }
-        setOutputImage(output);
-    }
-}
+import org.boblycat.blimp.jiuops.SuperSamplingScaleOperation;
 
 /**
  * A fast resize layer for e.g. zooming in the image view.
@@ -167,8 +65,7 @@ public class ViewResizeLayer extends DimensionAdjustmentLayer {
         if (zoomFactor.getMultiplier() == 1 && zoomFactor.getDivisor() == 1)
             return source;
         SuperSamplingScaleOperation scaler = new SuperSamplingScaleOperation();
-        scaler.multiplier = zoomFactor.getMultiplier();
-        scaler.divisor = zoomFactor.getDivisor();
+        scaler.setRatio(zoomFactor.getMultiplier(), zoomFactor.getDivisor());
         ret = new Bitmap(applyJiuOperation(ret.getImage(), scaler));
         double scaleFactor = source.getWidth() / (double) ret.getWidth();
         ret.setPixelScaleFactor(source.getPixelScaleFactor() * scaleFactor);
