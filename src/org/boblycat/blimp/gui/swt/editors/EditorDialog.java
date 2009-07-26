719b51b261bcdc8ec1bfa547185029649663645e
Knut Arild Erstad <knutae@gmail.com>
2009-07-26 18:26:30 +0200

    Factored out LayerEditorRegistry.DialogWrapper to a public class just named EditorDialog.

---
+++ src/org/boblycat/blimp/gui/swt/editors/EditorDialog.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2007, 2008, 2009 Knut Arild Erstad
+ *
+ * This file is part of Blimp, a layered photo editor.
+ *
+ * Blimp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Blimp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.boblycat.blimp.gui.swt.editors;
+
+import java.lang.reflect.Constructor;
+
+import org.boblycat.blimp.Serializer;
+import org.boblycat.blimp.Util;
+import org.boblycat.blimp.layers.Layer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Helper class for showing a layer editor dialog.
+ * @author Knut Arild Erstad
+ */
+public class EditorDialog {
+    private Layer originalClone;
+    private Layer workingClone;
+    private Layer actualLayer;
+    private Shell dialog;
+    private LayerEditorEnvironment env;
+    private Button previewCheckButton;
+    private LayerEditor editor;
+    private Shell parentShell;
+    private Constructor<? extends LayerEditor> editorConstructor;
+
+    private Layer editedLayer() {
+        if (previewEnabled())
+            return actualLayer;
+        else
+            return workingClone;
+    }
+
+    private boolean previewEnabled() {
+        return previewCheckButton.getSelection();
+    }
+    
+    public EditorDialog(Shell parentShell, Constructor<? extends LayerEditor> editorConstructor,
+            LayerEditorEnvironment environment) {
+        this.parentShell = parentShell;
+        this.editorConstructor = editorConstructor;
+        this.env = environment;
+    }
+
+    public void show() {
+        dialog = new Shell(parentShell, SWT.APPLICATION_MODAL
+                | SWT.TITLE | SWT.BORDER | SWT.RESIZE);
+        dialog.setLayout(new GridLayout());
+        dialog.setText(env.layer.getDescription());
+        ScrolledComposite wrapper = new ScrolledComposite(dialog,
+                SWT.V_SCROLL | SWT.H_SCROLL);
+        wrapper.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+        Object args[] = { wrapper, SWT.NONE };
+        editor = null;
+        try {
+            editor = editorConstructor.newInstance(args);
+        }
+        catch (Exception e) {
+            Util.err("Failed to construct editor", e);
+            return;
+        }
+        wrapper.setContent(editor);
+        wrapper.setExpandHorizontal(true);
+        wrapper.setExpandVertical(true);
+        wrapper.setMinSize(editor.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+        actualLayer = env.layer;
+        originalClone = (Layer) actualLayer.clone();
+        workingClone = (Layer) actualLayer.clone();
+
+        previewCheckButton = new Button(dialog, SWT.CHECK);
+        previewCheckButton.setText("Preview");
+        previewCheckButton.setSelection(editor.previewByDefault());
+        previewCheckButton.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event e) {
+                if (previewEnabled()) {
+                    // apply working changes to actual layer
+                    Serializer.copyBeanData(workingClone, actualLayer);
+                    if (env.layerWasJustAdded)
+                        actualLayer.setActive(true);
+                }
+                else {
+                    // update the working clone and revert the actual layer
+                    Serializer.copyBeanData(actualLayer, workingClone);
+                    Serializer.copyBeanData(originalClone, actualLayer);
+                    if (env.layerWasJustAdded)
+                        actualLayer.setActive(false);
+                }
+                editor.setLayer(editedLayer());
+                actualLayer.invalidate();
+            }
+        });
+
+        editor.session = env.session;
+        editor.workerThread = env.workerThread;
+        editor.setLayer(editedLayer());
+        if (env.layerWasJustAdded && previewEnabled()) {
+            actualLayer.setActive(true);
+            actualLayer.invalidate();
+        }
+
+        Composite buttonRow = new Composite(dialog, SWT.NONE);
+        buttonRow.setLayout(new FillLayout());
+        Button button = new Button(buttonRow, SWT.NONE);
+        button.setText("Ok");
+        button.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event e) {
+                if (editedLayer() != actualLayer)
+                    Serializer.copyBeanData(editedLayer(), actualLayer);
+                if (env.layerWasJustAdded)
+                    actualLayer.setActive(true);
+                actualLayer.invalidate();
+                editingFinished(false);
+            }
+        });
+        button = new Button(buttonRow, SWT.NONE);
+        button.setText("Cancel");
+        button.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event e) {
+                // revert layer changes
+                Serializer.copyBeanData(originalClone, actualLayer);
+                editingFinished(true);
+            }
+        });
+
+        dialog.pack();
+        dialog.open();
+    }
+
+    private void editingFinished(boolean cancelled) {
+        dialog.close();
+        dialog = null;
+        env.layerWasJustAdded = false;
+        if (env.editorCallback != null)
+            env.editorCallback.editingFinished(env, cancelled);
+    }
+}
--- src/org/boblycat/blimp/gui/swt/editors/LayerEditorRegistry.java
+++ src/org/boblycat/blimp/gui/swt/editors/LayerEditorRegistry.java
@@ -24,15 +24,7 @@ import java.util.Hashtable;
 
 import org.boblycat.blimp.*;
 import org.boblycat.blimp.layers.Layer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
 public class LayerEditorRegistry {
@@ -46,121 +38,7 @@ public class LayerEditorRegistry {
         }
 
         void showDialog(LayerEditorEnvironment environment) {
-            new DialogWrapper().showDialog(this, environment);
-        }
-    }
-
-    class DialogWrapper {
-        Layer originalClone;
-        Layer workingClone;
-        Layer actualLayer;
-        Shell dialog;
-        LayerEditorEnvironment env;
-        Button previewCheckButton;
-        LayerEditor editor;
-
-        Layer editedLayer() {
-            if (previewEnabled())
-                return actualLayer;
-            else
-                return workingClone;
-        }
-
-        boolean previewEnabled() {
-            return previewCheckButton.getSelection();
-        }
-
-        void showDialog(Entry entry, LayerEditorEnvironment environment) {
-            env = environment;
-            dialog = new Shell(parentShell, SWT.APPLICATION_MODAL
-                    | SWT.TITLE | SWT.BORDER | SWT.RESIZE);
-            dialog.setLayout(new GridLayout());
-            dialog.setText(env.layer.getDescription());
-            ScrolledComposite wrapper = new ScrolledComposite(dialog,
-                    SWT.V_SCROLL | SWT.H_SCROLL);
-            wrapper.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-            Object args[] = { wrapper, new Integer(SWT.NONE) };
-            editor = null;
-            try {
-                editor = entry.editorConstructor.newInstance(args);
-            }
-            catch (Exception e) {
-                Util.err("Failed to construct editor", e);
-                return;
-            }
-            wrapper.setContent(editor);
-            wrapper.setExpandHorizontal(true);
-            wrapper.setExpandVertical(true);
-            wrapper.setMinSize(editor.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-            actualLayer = env.layer;
-            originalClone = (Layer) actualLayer.clone();
-            workingClone = (Layer) actualLayer.clone();
-
-            previewCheckButton = new Button(dialog, SWT.CHECK);
-            previewCheckButton.setText("Preview");
-            previewCheckButton.setSelection(editor.previewByDefault());
-            previewCheckButton.addListener(SWT.Selection, new Listener() {
-                public void handleEvent(Event e) {
-                    if (previewEnabled()) {
-                        // apply working changes to actual layer
-                        Serializer.copyBeanData(workingClone, actualLayer);
-                        if (env.layerWasJustAdded)
-                            actualLayer.setActive(true);
-                    }
-                    else {
-                        // update the working clone and revert the actual layer
-                        Serializer.copyBeanData(actualLayer, workingClone);
-                        Serializer.copyBeanData(originalClone, actualLayer);
-                        if (env.layerWasJustAdded)
-                            actualLayer.setActive(false);
-                    }
-                    editor.setLayer(editedLayer());
-                    actualLayer.invalidate();
-                }
-            });
-
-            editor.session = env.session;
-            editor.workerThread = env.workerThread;
-            editor.setLayer(editedLayer());
-            if (env.layerWasJustAdded && previewEnabled()) {
-                actualLayer.setActive(true);
-                actualLayer.invalidate();
-            }
-
-            Composite buttonRow = new Composite(dialog, SWT.NONE);
-            buttonRow.setLayout(new FillLayout());
-            Button button = new Button(buttonRow, SWT.NONE);
-            button.setText("Ok");
-            button.addListener(SWT.Selection, new Listener() {
-                public void handleEvent(Event e) {
-                    if (editedLayer() != actualLayer)
-                        Serializer.copyBeanData(editedLayer(), actualLayer);
-                    if (env.layerWasJustAdded)
-                        actualLayer.setActive(true);
-                    actualLayer.invalidate();
-                    editingFinished(false);
-                }
-            });
-            button = new Button(buttonRow, SWT.NONE);
-            button.setText("Cancel");
-            button.addListener(SWT.Selection, new Listener() {
-                public void handleEvent(Event e) {
-                    // revert layer changes
-                    Serializer.copyBeanData(originalClone, actualLayer);
-                    editingFinished(true);
-                }
-            });
-
-            dialog.pack();
-            dialog.open();
-        }
-
-        void editingFinished(boolean cancelled) {
-            dialog.close();
-            dialog = null;
-            env.layerWasJustAdded = false;
-            if (env.editorCallback != null)
-                env.editorCallback.editingFinished(env, cancelled);
+            new EditorDialog(parentShell, editorConstructor, environment).show();
         }
     }
 
