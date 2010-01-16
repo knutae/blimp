772727a06152822c1bbb55a5707792180aebc0c0
Knut Arild Erstad <knutae@gmail.com>
2010-01-16 22:10:37 +0100

    Improved detection of scons.bat on windows, and added workaround for scons bug.

--- tools/prepare_jiu.py
+++ tools/prepare_jiu.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-import os, sys, subprocess
+import os, sys, subprocess, shutil
 
 # Public repos
 JIU_REPOS = 'http://jiu.hg.sourceforge.net:8000/hgroot/jiu/jiu'
 JIU_LOCAL_DIR = 'jiu'
 SCONS = 'scons'
 
+def find_scons_bat():
+    base = sys.exec_prefix
+    for path in [os.path.join(base, 'Scripts'), base]:
+        scons = os.path.join(path, 'scons.bat')
+        if os.path.exists(scons):
+            return scons
+    return 'scons.bat'
+
 if sys.platform == 'win32':
-    SCONS = 'scons.bat'
+    SCONS = find_scons_bat()
 
 def fetch():
     if os.path.isdir(JIU_LOCAL_DIR):
@@ -29,6 +37,9 @@ def build():
     here = os.getcwd()
     try:
         os.chdir(JIU_LOCAL_DIR)
+        if os.path.isdir('classes'):
+            # Workaround for scons bug (DirNodeInfo instance has no attribute 'csig')
+            shutil.rmtree('classes')
         subprocess.check_call([SCONS, 'jiu.jar'])
     finally:
         os.chdir(here)
